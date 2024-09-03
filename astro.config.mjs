db111364cf17ded4825d8f751366ed4abbda645a
danielscoffee <danielrsantoscontato@gmail.com>
2024-09-03 19:10:57 -0300

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon()]
+  integrations: [icon()],
+  output: 'static',
+  adapter: vercel(),
 });
