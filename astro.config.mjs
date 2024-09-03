b4e8209de271a4444d9930ec85cf824889c4c36c
danielscoffee <danielrsantoscontato@gmail.com>
2024-09-03 19:34:08 -0300

    Reverting commit to SSR Up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
+<<<<<<< HEAD
 import vercel from "@astrojs/vercel";
+=======
+import vercel from "@astrojs/vercel/static";
+>>>>>>> parent of 8ecd955 (SSR set up)
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
+<<<<<<< HEAD
   output: 'hybrid',
+=======
+  output: 'static',
+>>>>>>> parent of 8ecd955 (SSR set up)
   adapter: vercel(),
 });
