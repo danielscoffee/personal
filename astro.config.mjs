b18c7ee22124d1c773fb904039f685e45f211bd2
danielscoffee <danielrsantoscontato@gmail.com>
2024-09-04 17:35:35 -0300

    Removing Vercel Support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
-import vercel from "@astrojs/vercel/serverless";
-
 import mdx from "@astrojs/mdx";
 
 import netlify from "@astrojs/netlify";
